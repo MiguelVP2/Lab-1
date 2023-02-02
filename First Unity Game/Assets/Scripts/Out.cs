4ee61e76079803b3193f382cf99e6a28f8703ee1
Kaden Franklin <kadenfranklin@hotmail.com>
2023-02-02 16:04:02 -0600

    My game respawns meteors

---
+++ First Unity Game/Assets/Scripts/Out.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Out : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        print("collision with truck!");
+
+        if (col.gameObject.tag == "Respawn")
+        {
+            GameManager.instance.IncScore();
+            Destroy(col.gameObject);
+        }
+    }
+}
