5f6a7e089172ca74f53900e266bf71805e72552b
Kaden Franklin <kadenfranklin@hotmail.com>
2023-02-02 15:04:57 -0600

    prefab, made more drop scripts

--- First Unity Game/Assets/Scripts/Drop2.cs
+++ First Unity Game/Assets/Scripts/Drop2.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drop : MonoBehaviour
+public class Drop2 : MonoBehaviour
 {
-    private Rigidbody2D new_item;
+    private  Rigidbody2D new_item;
 
     // Start is called before the first frame update
     void Start()
     {
         new_item = GetComponent<Rigidbody2D>();
-        new_item.AddForce(transform.right * 140);
+        new_item.AddForce(transform.right * 120);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-}  
+}
